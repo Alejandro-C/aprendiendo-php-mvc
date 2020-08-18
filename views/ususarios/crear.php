ec63e754e9ea8e32dd902921d745512880ebcd6c
Ever Carvajal <62713105+Alejandro-C@users.noreply.github.com>
2020-08-18 13:56:54 -0500

    aprendiendo MVC

---
+++ views/ususarios/crear.php
@@ -0,0 +1,7 @@
+<form>
+    <label for="nombre">Nombre: </label>
+    <input type="text" name="nombre">
+
+    <label for="apellidos">Apellidos: </label>
+    <input type="text" name="apellidos">
+</form>
